c8793df1986f77361ff4686f903f54ea89312394
younes-karir <youneskarir@outlook.com>
2024-01-26 16:06:22 +0100

    using sequences

--- src/main/java/com/example/schoolmanagementsystem/model/Student.java
+++ src/main/java/com/example/schoolmanagementsystem/model/Student.java
@@ -12,18 +12,32 @@ import lombok.NoArgsConstructor;
 @AllArgsConstructor
 @NoArgsConstructor
 @Builder
-@Table(name = "students")
+@Table(
+        name = "student" ,
+        uniqueConstraints = @UniqueConstraint(
+            name = "email_unique",
+            columnNames = "student-email"
+        )
+)
 public class Student {
 
     @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @GeneratedValue(
+            strategy = GenerationType.SEQUENCE,
+            generator = "student_sequence"
+    )
+    @SequenceGenerator(
+            name = "student_sequence",
+            sequenceName = "student_sequence",
+            allocationSize = 1
+    )
     @Column (name = "student-id")
     private long studentId;
-    @Column (name = "student-first-name")
+    @Column (name = "student-firstName")
     private String studentFistName;
-    @Column (name = "student-last-name")
+    @Column (name = "student-lastName")
     private String studentLastName;
-    @Column (name = "student-email")
+    @Column (name = "student-email" ,nullable = false, unique = true)
     private String studentEmail;
 
 }
