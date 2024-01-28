856e9a032251f9c7433c115422783faabaaa5bd4
younes-karir <youneskarir@outlook.com>
2024-01-28 13:49:16 +0100

    Custom Query Methods + native Query methods

--- src/main/java/com/example/schoolmanagementsystem/repository/StudentRepository.java
+++ src/main/java/com/example/schoolmanagementsystem/repository/StudentRepository.java
@@ -3,6 +3,8 @@ package com.example.schoolmanagementsystem.repository;
 import com.example.schoolmanagementsystem.model.Guardian;
 import com.example.schoolmanagementsystem.model.Student;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import java.security.Guard;
@@ -16,9 +18,42 @@ public interface StudentRepository extends JpaRepository<Student, Long> {
     List<Student> findByStudentFistNameContaining(String Arg);
     List<Student> findAllByGuardian(Guardian guardian);
 
-    List<Student> findByStudentFistNameNotNull(String name);
 
-    List<Student> findByGuardianName(String name);
+
+    // creating custom queries JPQL
+
+
+    //get student by email
+    @Query("select s from Student s WHERE s.studentEmail = ?1")
+    Student getStudentByStudentEmail(String s);
+
+    //get student's first name by email
+    @Query("select s.studentFistName from Student s WHERE s.studentEmail = ?1")
+    String getStudentFirstNameByStudentEmail(String s);
+
+
+
+
+    // creating native queries -----------------
+
+    //get student name by email
+
+    @Query(
+            value = "select * from student s where s.student_email = ?1 ",
+            nativeQuery = true
+    )
+    Student getStudentByStudentEmailNative(String email);
+
+
+    //get student name by email
+
+    @Query(
+            value = "select * from student s where s.student_email = :studentEmail ",
+            nativeQuery = true
+    )
+    Student getStudentByStudentEmailNativeNamedParam(
+            @Param("studentEmail") String email
+    );
 
 
 
