0df28461b9ed3e8a1a93eaf77aae8e0e9087a403
younes-karir <youneskarir@outlook.com>
2024-01-31 18:52:58 +0100

    getting list of courses

---
+++ src/test/java/com/example/schoolmanagementsystem/repository/CourseRepositoryTest.java
@@ -0,0 +1,28 @@
+package com.example.schoolmanagementsystem.repository;
+
+import com.example.schoolmanagementsystem.model.Course;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+
+@SpringBootTest
+class CourseRepositoryTest {
+
+
+    @Autowired
+    CourseRepository courseRepository;
+
+
+
+    @Test
+    public void getAllCourses(){
+        List<Course> data = courseRepository.findAll();
+        System.out.println("courses = " + data);
+    }
+
+}
