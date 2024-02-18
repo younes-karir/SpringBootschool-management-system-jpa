764ad7f9c49081a7fec6e471a9104ab9a9a286d3
younes-karir <youneskarir@outlook.com>
2024-02-18 19:08:47 +0100

    adding custom methods to teacher repository

--- src/main/java/com/example/schoolmanagementsystem/repository/TeacherRepository.java
+++ src/main/java/com/example/schoolmanagementsystem/repository/TeacherRepository.java
@@ -7,4 +7,6 @@ import org.springframework.stereotype.Repository;
 
 @Repository
 public interface TeacherRepository extends JpaRepository<Teacher,Long> {
+
+    Teacher findTeacherByEmail(String email);
 }
