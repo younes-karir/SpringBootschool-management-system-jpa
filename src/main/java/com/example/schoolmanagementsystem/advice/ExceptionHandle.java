0762af964415c3a80a052a5ec66a8f684a0052cc
younes-karir <youneskarir@outlook.com>
2024-02-21 14:44:44 +0100

    update exception handler

--- src/main/java/com/example/schoolmanagementsystem/advice/ExceptionHandle.java
+++ src/main/java/com/example/schoolmanagementsystem/advice/ExceptionHandle.java
@@ -7,6 +7,8 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RequestHeader;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
 import java.util.HashMap;
@@ -17,6 +19,7 @@ public class ExceptionHandle {
 
 
     @ExceptionHandler(MethodArgumentNotValidException.class)
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
     public Map<String,String> handleException(MethodArgumentNotValidException exceptions){
         Map<String,String> errors = new HashMap<>();
         exceptions.getBindingResult().getFieldErrors().forEach(
